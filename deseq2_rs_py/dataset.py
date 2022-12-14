475bfee55838a3dfcc63f97dc8de5b62950e0ec6
Mihir Samdarshi <msamdars@stanford.edu>
2022-12-15 01:20:31 +0530

    chore: fix Pylint errors

--- deseq2_rs_py/dataset.py
+++ deseq2_rs_py/dataset.py
@@ -4,7 +4,11 @@ import pandas as pd
 
 
 class Dataset:
-    def __init__(self, count_data: pd.DataFrame, col_data: pd.DataFrame, design_col:str):
+    """
+    A Dataset object is a container for count data and column data. It is used to
+    initialize a DESeq2 object.
+    """
+    def __init__(self, count_data: pd.DataFrame, col_data: pd.DataFrame, design_col: str):
         self.count_data = count_data
         self.col_data = col_data
         self.design_col = design_col
@@ -13,7 +17,9 @@ class Dataset:
 
     def _data_checks(self):
         if not self.count_data.shape[1] == self.col_data.shape[0]:
-            raise ValueError("Count data must have the same number of columns as the column data has rows")
+            raise ValueError(
+                "Count data must have the same number of columns as the column data has rows"
+            )
         if self.col_data.isna().any().any():
             raise ValueError("Column data cannot contain NaN")
         if self.col_data.isnull().any().any():
